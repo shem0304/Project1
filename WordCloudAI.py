1efe03ef1aadbf359b3fe2cc3fd9229902b13724
shem0304 <shem0304@gmail.com>
2020-03-11 23:02:12 +0900

    v2.0

--- WordCloudAI.py
+++ WordCloudAI.py
@@ -29,7 +29,7 @@ def showGraph(wordInfo):
 # 출처 : https://thinkwarelab.wordpress.com/2016/08/30/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%98%95%ED%83%9C%EC%86%8C-%EB%B6%84%EC%84%9D%EC%9C%BC%EB%A1%9C-%EC%9B%8C%EB%93%9C%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%EA%B7%B8%EB%A6%AC%EA%B8%B0/
 
 def saveWordCloud(wordInfo, filename):
-    taglist = pytagcloud.make_tags(dict(wordInfo).items(), maxsize=150)
+    taglist = pytagcloud.make_tags(dict(wordInfo).items(), maxsize=50)
     pytagcloud.create_tag_image(taglist, filename, size=(640, 480), fontname='korean', rectangular=False)
     webbrowser.open(filename)
 
@@ -45,10 +45,11 @@ def main():
     print(nouns)
     count = Counter(nouns)
 
+    removewords = ['바이오', '산업', '위해', '방문', '부처', '등', '처', '한국']
     #그래프
     wordInfo = dict()
     for tags, counts in count.most_common(30):
-        if (len(str(tags)) > 1):
+        if (len(str(tags)) > 1) and not tags in (removewords):
             wordInfo[tags] = counts
             print("%s : %d" % (tags, counts))
 
