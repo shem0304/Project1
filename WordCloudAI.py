89955cdd0e6aa8daf35512e2b891736303853551
shem0304 <shem0304@gmail.com>
2020-03-11 22:27:22 +0900

    v1.0

--- WordCloudAI.py
+++ WordCloudAI.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from konlpy.tag import Twitter
+from konlpy.tag import Twitter, Okt
 import pytagcloud
 import matplotlib.image as img
 import matplotlib.pyplot as pp
@@ -29,7 +29,7 @@ def showGraph(wordInfo):
 # 출처 : https://thinkwarelab.wordpress.com/2016/08/30/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%98%95%ED%83%9C%EC%86%8C-%EB%B6%84%EC%84%9D%EC%9C%BC%EB%A1%9C-%EC%9B%8C%EB%93%9C%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%EA%B7%B8%EB%A6%AC%EA%B8%B0/
 
 def saveWordCloud(wordInfo, filename):
-    taglist = pytagcloud.make_tags(dict(wordInfo).items(), maxsize=80)
+    taglist = pytagcloud.make_tags(dict(wordInfo).items(), maxsize=150)
     pytagcloud.create_tag_image(taglist, filename, size=(640, 480), fontname='korean', rectangular=False)
     webbrowser.open(filename)
 
@@ -40,14 +40,14 @@ def main():
 
     print(data)
 
-    nlp = Twitter()
+    nlp = Okt()
     nouns = nlp.nouns(data)
     print(nouns)
     count = Counter(nouns)
 
     #그래프
     wordInfo = dict()
-    for tags, counts in count.most_common(20):
+    for tags, counts in count.most_common(30):
         if (len(str(tags)) > 1):
             wordInfo[tags] = counts
             print("%s : %d" % (tags, counts))
